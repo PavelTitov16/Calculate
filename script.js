554295f20bd74ba281a7409768333749c1e04d88
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-17 20:17:31 +0300

    feat: add calculate iterations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const incomeSalary = document.getElementById('income-salary');
 const incomeFreelance = document.getElementById('income-freelance');
 const incomeExtra1 = document.getElementById('income-extra-1');
-const incomeExtra1 = document.getElementById('income-extra-2');
+const incomeExtra2 = document.getElementById('income-extra-2');
 
 const rentFlat = document.getElementById('costs-flat');
 const costHouseServices = document.getElementById('costs-house-services');
@@ -18,3 +18,25 @@ const spend = document.getElementById('spend');
 
 let accumulation = 0;
 let totalPrecents = 0;
+
+const inputs = document.querySelectorAll('.input');
+
+let totalMonth;
+let totalDay;
+let totalYear;
+
+for (input of inputs) {
+    input.addEventListener('input', () => {
+        countAvaliableMoney();
+    })
+}
+
+const strToNum = str => str.value ? parseInt(str.value) : 0;
+
+const countAvaliableMoney = () => {
+    const totalPerMonth = strToNum(incomeSalary) + strToNum(incomeFreelance) + strToNum(incomeExtra1) + strToNum(incomeExtra2);
+    const totalCosts = strToNum(rentFlat) + strToNum(costHouseServices) + strToNum(priceTransport) + strToNum(loanPrice);
+
+    totalMonth = totalPerMonth - totalCosts;
+    totalMonthInput.value = totalMonth;
+}
