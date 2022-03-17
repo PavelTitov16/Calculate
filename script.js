08a92581aa7e02ff544c94627f7c4e9b643ebfa6
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-17 19:23:45 +0300

    feat: add moneybox with inputs

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const rentFlat = document.getElementById('costs-flat');
 const costHouseServices = document.getElementById('costs-house-services');
 const priceTransport = document.getElementById('costs-transport');
 const loanPrice = document.getElementById('costs-credit');
+
+const totalMonthInput = document.getElementById('total-month');
+const totalDayInput = document.getElementById('total-day');
+const totalYearInput = document.getElementById('total-year');
+
+const moneyBox = document.getElementById('money-box-range');
+const accumulationInput = document.getElementById('accumulation');
+const spend = document.getElementById('spend');
+
+let accumulation = 0;
+let totalPrecents = 0;
