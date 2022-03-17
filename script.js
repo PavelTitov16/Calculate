807eec8dcb51636265d5b9511af3e3679579d5c0
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-17 19:18:43 +0300

    feat: add spend inputs

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const incomeSalary = document.getElementById('income-salary');
 const incomeFreelance = document.getElementById('income-freelance');
 const incomeExtra1 = document.getElementById('income-extra-1');
 const incomeExtra1 = document.getElementById('income-extra-2');
+
+const rentFlat = document.getElementById('costs-flat');
+const costHouseServices = document.getElementById('costs-house-services');
+const priceTransport = document.getElementById('costs-transport');
+const loanPrice = document.getElementById('costs-credit');
