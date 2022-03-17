8fa18d8213925a858704e7806979ee04ed14127e
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-17 19:16:18 +0300

    feat: add income inputs

---
+++ script.js
@@ -0,0 +1,4 @@
+const incomeSalary = document.getElementById('income-salary');
+const incomeFreelance = document.getElementById('income-freelance');
+const incomeExtra1 = document.getElementById('income-extra-1');
+const incomeExtra1 = document.getElementById('income-extra-2');
